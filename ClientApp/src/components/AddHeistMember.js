e95c3bb5de8f0b6ba70bbf0727d926cd02143d02
Suzana Hersak <shersak@net.hr>
2021-11-07 14:25:45 +0100

    Add files via upload

---
+++ ClientApp/src/components/AddHeistMember.js
@@ -0,0 +1,142 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import { Link, NavLink } from 'react-router-dom';
+import { HeistMemberData } from './FetchHeistMember';
+
+interface AddHeistMemberData {
+    title: string;
+    loading: boolean;
+    skillsList: Array<any>;
+    heistData: HeistMemberData;
+}
+
+export class AddHeistMember extends React.Component<RouteComponentProps<{}>, AddHeistMemberData> {
+    constructor(props) {
+        super(props);
+
+        this.state = { title: "", loading: true, heistMemberList: [], heistData: new HeistMemberData };
+
+        fetch('api/member/GetHeistMemberList')
+            .then(response => response.json() as Promise<Array<any>>)
+            .then(data => {
+                this.setState({ skillsList: data });
+            });
+
+        var hmid = this.props.match.params["hmid"];
+
+        // Edit heist member
+        if (hmid > 0) {
+            fetch('api/member/DetailsHeistMember/' + hmid)
+                .then(response => response.json() as Promise<HeistMemberData>)
+                .then(data => {
+                    this.setState({ title: "EditHeistMember", loading: false, heistData: data });
+                });
+        }
+
+        // Add heist member
+        else {
+            this.state = { title: "CreateHeistMember", loading: false, skillsList: [], heistData: new HeistMemberData };
+        }
+
+        // This binding is necessary to make "this" work in the callback  
+        this.handleSave = this.handleSave.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+    }
+
+    public render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : this.renderCreateForm(this.state.skillsList);
+
+        return <div>
+            <h1>{this.state.title}</h1>
+            <h3>Heist Member</h3>
+            <hr />
+            {contents}
+        </div>;
+    }
+
+    // This will handle the submit form event 
+    private handleSave(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+
+        // PUT request for Edit Heist member 
+        if (this.state.heistData.heistMemberId) {
+            fetch('api/member/EditHeistMember', {
+                method: 'PUT',
+                body: data,
+
+            }).then((response) => response.json())
+                .then((responseJson) => {
+                    this.props.history.push("/fetchheistmember");
+                })
+        }
+
+        // POST request for Add Heist member
+        else {
+            fetch('api/member/CreateHeistMember', {
+                method: 'POST',
+                body: data,
+
+            }).then((response) => response.json())
+                .then((responseJson) => {
+                    this.props.history.push("/fetchheistmember");
+                })
+        }
+    }
+
+    // This will handle Cancel button click event
+    private handleCancel(e) {
+        e.preventDefault();
+        this.props.history.push("/fetchheistmember");
+    }
+
+    // Method eturns the HTML Form 
+    private renderCreateForm(skillList: Array<any>) {
+        return (
+            <form onSubmit={this.handleSave} >
+                <div className="form-group row" >
+                    <input type="hidden" name="heistMemberId" value={this.state.heistData.heistMemberId} />
+                </div>
+                < div className="form-group row" >
+                    <label className=" control-label col-md-12" htmlFor="Name">Name</label>
+                    <div className="col-md-4">
+                        <input className="form-control" type="text" name="name" defaultValue={this.state.heistData.name} required />
+                    </div>
+                </div >
+                <div className="form-group row">
+                    <label className="control-label col-md-12" htmlFor="Sex">Sex</label>
+                    <div className="col-md-4">
+                        <select className="form-control" data-val="true" name="sex" defaultValue={this.state.heistData.sex} required>
+                            <option value="">-- Select Sex --</option>
+                            <option value="M">M</option>
+                            <option value="F">F</option>
+                        </select>
+                    </div>
+                </div >
+                <div className="form-group row">
+                    <label className="control-label col-md-12" htmlFor="Email" >Email</label>
+                    <div className="col-md-4">
+                        <input className="form-control" type="text" name="Email" defaultValue={this.state.heistData.emial} required />
+                    </div>
+                </div>
+                <div className="form-group row">
+                    <label className="control-label col-md-12" htmlFor="Skills">Skills</label>
+                    <div className="col-md-4">
+                        <select className="form-control" data-val="true" name="Skills" defaultValue={this.state.heistData.skills} required>
+                            <option value="">-- Select Skills --</option>
+                            {skillsList.map(skills =>
+                                <option key={skills.heistSkillsId} value={skills.name}>{skills.level}</option>
+                            )}
+                        </select>
+                    </div>
+                </div >
+                <div className="form-group">
+                    <button type="submit" className="btn btn-default">Save</button>
+                    <button className="btn" onClick={this.handleCancel}>Cancel</button>
+                </div >
+            </form >
+        )
+    }
+}
---
+++ ClientApp/src/components/FetchHeistMember.js
@@ -0,0 +1,109 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import { Link, NavLink } from 'react-router-dom';
+
+interface FetchHeistMemberDataState {
+    heistMemberList: HeistMemberData[];
+    loading: boolean;
+}
+
+export class FetchHeistMember extends React.Component<RouteComponentProps<{}>, FetchHeistMemberDataState> {
+    constructor() {
+        super();
+        this.state = { heistMemberList: [], loading: true };
+
+        fetch('api/member/Index')
+            .then(response => response.json() as Promise<HeistMemberData[]>)
+            .then(data => {
+                this.setState({ heistMemberList: data, loading: false });
+            });
+
+        // This binding is necessary to make "this" work in the callback
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+
+    }
+
+    public render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : this.renderHeistMemberTable(this.state.heistMemberList);
+
+        return <div>
+            <h1>Heist member Data</h1>
+            <p>
+                <Link to="/addHeistMember">Create New Heist member</Link>
+            </p>
+            {contents}
+        </div>;
+    }
+
+    // Delete request for member
+    private handleDelete(id: number) {
+        if (!confirm("Do you want to delete heist member with Id: " + id))
+            return;
+        else {
+            fetch('api/member/deleteHeistMember/' + id, {
+                method: 'deleteHeistMember'
+            }).then(data => {
+                this.setState(
+                    {
+                        heistMemberList: this.state.heistMemberList.filter((rec) => {
+                            return (rec.heistMemberId != id);
+                        })
+                    });
+            });
+        }
+    }
+
+    private handleEdit(id: number) {
+        this.props.history.push("/member/EditHeistMember/" + id);
+    }
+
+    // Render() method returns the html table
+    private renderHeistMemberTable(heistMemberList: HeistMemberData[]) {
+        return <table className='table'>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>HeistMemberId</th>
+                    <th>Name</th>
+                    <th>Sex</th>
+                    <th>Email</th>
+                    <th>Skills</th>
+                    <th>MainSkill</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                {heistMemberList.map(emp =>
+                    <tr key={emp.heistMemberId}>
+                        <td></td>
+                        <td>{emp.heistMemberId}</td>
+                        <td>{emp.name}</td>
+                        <td>{emp.sex}</td>
+                        <td>{emp.email}</td>
+                        <td>{emp.skills}</td>
+                        <td>
+                            <a className="action" onClick={(id) => this.handleEdit(emp.heistMemberId)}>Edit</a>  |
+                            <a className="action" onClick={(id) => this.handleDelete(emp.heistMemberId)}>Delete</a>
+                        </td>
+                        <td>{emp.mainSkill}</td>
+                        <td>{emp.status}</td>
+                    </tr>
+                )}
+            </tbody>
+        </table>;
+    }
+}
+
+
+export class HeistMemberData {
+    heistMemberId: number = 0;
+    name: string = "";
+    sex: string = "";
+    email: string = "";
+    skills: string = "";
+    mainSkill: string = "";
+    status: string = "";
+}
