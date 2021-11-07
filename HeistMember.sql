cc8b2ad039721b2df9ce7317a7ea1dd09a42b3fa
Suzana Hersak <shersak@net.hr>
2021-11-07 14:38:54 +0100

    Add files via upload

---
+++ HeistMember.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[HeistMember]
+(
+	[HeistMemberId] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(30) NULL, 
+    [Sex] NCHAR(1) NULL, 
+    [Email] NCHAR(30) NULL, 
+    [Skills] NCHAR(20) NULL, 
+    [MainSkill] NCHAR(20) NULL, 
+    [Status] NCHAR(20) NULL
+)
---
+++ HeistSkills.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[HeistSkills]
+(
+	[HeistSkillsId] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(30) NULL, 
+    [Level] NCHAR(10) NULL
+)
